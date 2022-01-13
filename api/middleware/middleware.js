e58be7c0f098db531403b53432fa7d79eebb324c
Bret Banger <Banger484@gmail.com>
2022-01-12 19:04:28 -0600

    finished working through all tests

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,79 @@
+const User = require('../users/users-model')
+const Post = require('../posts/posts-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const currentDateTime = new Date();
+  const formattedDate =
+    currentDateTime.getFullYear() +
+    "-" +
+    (currentDateTime.getMonth() + 1) +
+    "-" +
+    currentDateTime.getDate() +
+    "-" +
+    currentDateTime.getHours() +
+    ":" +
+    currentDateTime.getMinutes() +
+    ":" +
+    currentDateTime.getSeconds()
+
+  const method = req.method
+  const url = req.url
+  const status = res.statusCode
+
+  const log = `[${formattedDate}] ${method}:${url} ${status}`
+  console.log(log)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
-const server = express();
+const userRouter = require('./users/users-router')
+const { 
+  logger, 
+  validateUserId, 
+  validateUser, 
+  validatePost,
+} = require('./middleware/middleware')
+// global middlewares and the user's router need to be connected here
 
+const server = express();
 // remember express by default cannot parse JSON in request bodies
 
-// global middlewares and the user's router need to be connected here
+server.use(express.json())
+
+server.use('/api/users', userRouter)
+
+
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
